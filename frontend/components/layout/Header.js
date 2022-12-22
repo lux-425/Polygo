1f0d45485bace6b93c0510e4a68e526c54e53612
luke <garcia.luc66@gmail.com>
2022-12-22 19:30:15 +0100

    Header's user profile banner

--- frontend/components/layout/Header.js
+++ frontend/components/layout/Header.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useContext } from 'react';
+
+import Image from 'next/image';
+import userLogo from '/public/katana.png';
+
+import AuthContext from '../../context/AuthContext';
 
 const Header = () => {
+  const { user } = useContext(AuthContext);
+
+  if (user) user.level = 5;
+
   return (
     <div id='header'>
-      <a href='/' id='polygo'>Polygo</a>
-    </div>
-  )
-}
+      <a href='/' id='polygo'>
+        Polygo
+      </a>
 
-export default Header
+      <div className='modal userProfile'>
+        <Image id='userLogo' width={33} height={33} alt='Logo' src={userLogo} />
+        <span>&nbsp;</span>
+        <div>
+          {user && user.username}
+          <br />
+          Level : {user && user.level}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
